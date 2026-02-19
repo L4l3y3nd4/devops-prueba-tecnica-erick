5cf3a1c661c9f801e1ea66c826dabc44184af023
Erick <erickemeldy@gmail.com>
2026-02-18 22:09:43 -0400

    Fix ALLOWED_HOSTS, improve Dockerfile and add Kubernetes manifests

--- demo/settings.py
+++ demo/settings.py
@@ -16,7 +16,9 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+import os
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
